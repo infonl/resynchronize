cfc33cfc8ca5e9c2e50da90a300eba7e03e26ed2
Victor Kushchenko <victor@info.nl>
2019-05-21 12:48:30 +0200

    Add some tests for 'getAsyncKeys' function and refactor it

--- src/lib/getGetterAsyncProps.test.js
+++ src/lib/getGetterAsyncProps.test.js
@@ -1,4 +1,4 @@
-/* global test, expect */
+/* global describe, test, expect */
 const getGetterAsyncProps = require('./getGetterAsyncProps')
 
 test('getGetterAsyncProps returns the properties located on the getter prop with an object with 4 basic properties for each one', () => {
@@ -17,4 +17,29 @@ test('getGetterAsyncProps returns the properties located on the getter prop with
   expect(properties.asyncProp).toHaveProperty('loading')
   expect(properties.asyncProp).toHaveProperty('done')
   expect(properties.asyncProp).toHaveProperty('error')
+})
+
+describe('getAsyncKeys', () => {
+  test('should return keys of getter object', () => {
+    const getter = {
+      asyncProp1: () => {},
+      asyncProp2: () => {}
+    }
+
+    const keys = getGetterAsyncProps.getAsyncKeys(getter)
+    expect(keys).toEqual(['asyncProp1', 'asyncProp2'])
+  })
+  test('should return empty list of keys if getter object empty', () => {
+    const keys = getGetterAsyncProps.getAsyncKeys({})
+    expect(keys).toEqual([])
+  })
+  test('should return default list of keys if getter is null', () => {
+    const keys = getGetterAsyncProps.getAsyncKeys(null)
+    expect(keys).toEqual(['asyncProp'])
+  })
+
+  test('should return default list of keys if getter is undefined', () => {
+    const keys = getGetterAsyncProps.getAsyncKeys()
+    expect(keys).toEqual(['asyncProp'])
+  })
 })
