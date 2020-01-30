29632b78e36869f610be91f8b1ae534bc82b39cf
Marcos Miani <marcos@info.nl>
2020-01-30 14:59:45 +0100

    Update site config

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,15 +23,15 @@ const users = [
 const siteConfig = {
   title: 'Resynchronize', // Title for your website.
   tagline: 'The guide to the tools for asynchronous behaviour on your state',
-  url: 'https://infonl.github.io/resynchronize/', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  url: 'https://infonl.github.io/', // Your website URL
+  baseUrl: '/resynchronize/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'Resynchronize',
-  organizationName: 'Info.nl',
+  projectName: 'resynchronize',
+  organizationName: 'info.nl',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
